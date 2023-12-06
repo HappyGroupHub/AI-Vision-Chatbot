a48a1cf690965007675c44ea29c43e9af80e0249
LD <chiyc88@gmail.com>
2023-12-06 17:58:38 +0800

    Update - the chat console is now displayed normally and accept user uploading image

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,67 @@
   <div id="app">
     <h1>Chat Bot</h1>
     <div class="chat-panel">
-      <div class="messages">
-        <div v-for="(message, index) in chatHistory" :key="index" :class="{ 'user-message': message.fromUser, 'bot-message': !message.fromUser }">
-          <div v-if="message.fromUser" class="message user-message-right">{{ message.text }}</div>
-          <div v-else class="message bot-message-left">{{ message.text }}</div>
+      <div class="messages" :style="{ maxHeight: messagesMaxHeight }">
+        <div v-for="(message, index) in chatHistory" :key="index" class="message-container">
+          <template v-if="message.fromUser && message.isImage">
+            <!-- User's image message -->
+            <div class="message user-message-right">
+              <div class="message-header">
+                <div class="sender-info">
+                  <span class="sender-name">User</span>
+                  <img class="sender-icon" src="./assets/user_icon.png" alt="User Icon"/>
+                </div>
+              </div>
+              <p v-if="message.text">{{ message.text }}</p>
+              <img v-if="message.imageUrl" :src="message.imageUrl" alt="Uploaded Image" class="uploaded-image" />
+            </div>
+          </template>
+          <template v-else-if="message.fromUser">
+            <!-- User's text message -->
+            <div class="message user-message-right">
+              <div class="message-header">
+                <div class="sender-info">
+                  <span class="sender-name">User</span>
+                  <img class="sender-icon" src="./assets/user_icon.png" alt="User Icon"/>
+                </div>
+              </div>
+              {{ message.text }}
+            </div>
+          </template>
+          <template v-else>
+            <!-- Bot's response -->
+            <div class="message bot-message-left">
+              <div class="message-header">
+                <div class="sender-info">
+                  <img class="sender-icon" src="./assets/bot_icon.png" alt="Bot Icon"/>
+                  <span class="sender-name">Bot's Response</span>
+                </div>
+              </div>
+              {{ message.text }}
+            </div>
+          </template>
         </div>
       </div>
-      <input v-model="userInput" @keyup.enter="sendMessage" placeholder="Type your message..." />
+      <div class="input-container">
+        <input v-model="userInput" @keyup.enter="sendMessage" placeholder="Type your message..."/>
+        <button class="upload-icon" @click="openFileInput">📷</button>
+        <div class="send-icon" @click="sendMessage">➤</div>
+        <!-- Hidden file input for image selection -->
+        <input ref="fileInput" type="file" accept="image/*" @change="handleImageUpload" style="display: none" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import {sendMessageToBackend} from "./services/backendServices.js";
+import { sendMessageToBackend } from "./services/backendServices.js";
 
 export default {
   data() {
     return {
       userInput: '',
       chatHistory: [],
+      messagesMaxHeight: 'calc(70vh - 3rem)', // Adjust the value as needed
     };
   },
   methods: {
@@ -36,9 +78,28 @@ export default {
       // const response = await sendMessageToBackend(this.userInput);
       this.receiveMessage('This is a sample response from the bot.');
     },
-    receiveMessage(text) {
-      // Add the bot's response to the chat history
-      this.chatHistory.push({ text, fromUser: false });
+    receiveMessage(text, imageUrl, isImage = false) {
+      if (isImage) {
+        // Add a separate message for the image description
+        this.chatHistory.push({ text, fromUser: true });
+        // Add the image message to the chat history with a user flag
+        this.chatHistory.push({ imageUrl, fromUser: true, isImage });
+      } else {
+        // Add the bot's response to the chat history
+        this.chatHistory.push({ text, fromUser: false });
+      }
+    },
+    openFileInput() {
+      // Trigger the file input
+      this.$refs.fileInput.click();
+    },
+    handleImageUpload(event) {
+      const file = event.target.files[0];
+      if (file) {
+        // Handle image upload logic
+        const imageUrl = URL.createObjectURL(file);
+        this.receiveMessage(`User uploaded an image: ${file.name}`, imageUrl, true);
+      }
     },
   },
 };
@@ -57,20 +118,26 @@ h1 {
 .chat-panel {
   border: 1px solid #ccc;
   padding: 1rem;
-  width: 400px; /* Increase the width */
+  width: 70vw; /* Use viewport width */
   margin: auto;
   border-radius: 8px;
+  box-sizing: border-box;
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
 }
 
 .messages {
-  max-height: 400px; /* Increase the height */
   overflow-y: auto;
+}
+
+.message-container {
+  margin-bottom: 0.5rem;
 }
 
 .message {
   padding: 0.5rem;
   border-radius: 8px;
-  margin-bottom: 0.5rem;
 }
 
 .user-message-right {
@@ -83,9 +150,73 @@ h1 {
   background-color: #f0f0f0;
 }
 
+.message-header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.sender-info {
+  display: flex;
+  align-items: center;
+}
+
+.sender-name {
+  font-weight: bold;
+  margin-left: 5px;
+}
+
+.sender-icon {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
+
+.input-container {
+  display: flex;
+  margin-top: auto;
+}
+
 input {
-  width: 100%;
-  padding: 0.5rem;
-  margin-top: 0.5rem;
+  flex-grow: 1;
+}
+
+.send-icon {
+  cursor: pointer;
+  font-size: 1rem;
+  margin-left: 5px;
+}
+
+/* Adjust the styles for the sender info within user-message-right */
+.user-message-right .message-header {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.user-message-right .sender-info {
+  margin-left: 5px;
+}
+
+.user-message-right .sender-name {
+  font-weight: bold;
+  margin-right: 5px;
+}
+
+.user-message-right .sender-icon {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
+
+.upload-icon {
+  cursor: pointer;
+  font-size: 1rem;
+  margin-right: 5px;
+}
+
+.uploaded-image {
+  max-width: 100%;
+  max-height: 200px;
+  border-radius: 8px;
+  margin-top: 5px;
 }
 </style>
